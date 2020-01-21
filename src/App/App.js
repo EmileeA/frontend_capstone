721dae06678e5172e26e33c5c3ab9ec0f35c9f8c
Emilee Mitchell <emcakes@gmail.com>
2020-01-21 15:11:27 -0600

    I have a login button and was able to grab my UID to populate my json. I then imported that file into the DB

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import NavBar from '../components/shared/NavBar/NavBar';
+import firebaseConnection from '../helpers/data/connection';
+// import Home from '../components/pages/Home/Home';
+import Auth from '../components/pages/Auth/Auth';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-    </div>
-  );
-}
+firebaseConnection();
 
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+    <div className="App">
+      <NavBar authed={authed} />
+      <Auth authed={authed} />
+      </div>
+    );
+  }
+}
 export default App;
